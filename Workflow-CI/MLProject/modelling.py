119afe636f3556c8979a904b1de67e8ccdc9b297
FanyaDs <2218068@scholar.itn.ac.id>
2025-10-12 17:55:07 +0000

    Fix: remove mlflow.start_run to prevent Run not found error (final)

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -12,7 +12,6 @@ if __name__ == "__main__":
     X, y = load_iris(return_X_y=True)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    with mlflow.start_run():
         model = RandomForestClassifier(n_estimators=50, random_state=42)
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
