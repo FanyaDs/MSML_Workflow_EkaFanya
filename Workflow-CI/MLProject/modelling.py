7a4a8f521cc17ab4019327abe77f0160e54fb978
FanyaDs <2218068@scholar.itn.ac.id>
2025-10-12 17:56:43 +0000

    Fix: remove extra indentation after deleting mlflow.start_run

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -6,11 +6,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
-    mlflow.set_tracking_uri("file:///tmp/mlruns")
-    mlflow.set_experiment("ci_training")
+mlflow.set_tracking_uri("file:///tmp/mlruns")
+mlflow.set_experiment("ci_training")
 
     X, y = load_iris(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
         model = RandomForestClassifier(n_estimators=50, random_state=42)
         model.fit(X_train, y_train)
