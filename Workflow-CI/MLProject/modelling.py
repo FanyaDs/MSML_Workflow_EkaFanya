8f732a2938c5bc0cac7b9bd5e80221dd6c06028a
FanyaDs <2218068@scholar.itn.ac.id>
2025-10-12 18:02:32 +0000

    Fix: correct indentation of main block (final)

--- Workflow-CI/MLProject/modelling.py
+++ Workflow-CI/MLProject/modelling.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-    if __name__ == "__main__":
+if __name__ == "__main__":
     mlflow.set_tracking_uri("file:///tmp/mlruns")
     mlflow.set_experiment("ci_training")
     
